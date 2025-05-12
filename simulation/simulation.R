bf91f8fb4630dd88e22c4261bac94f12dd717505
Lasse Meixner <lassemeixner@gmail.com>
2025-05-12 17:50:18 +0200

    1k sim size

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -12,7 +12,7 @@ results <- run_simulation_parallel(
   P = 100,
   setting = "fixed",
   sigma = c(1, 2, 4),
-  simulation_size = 500,
+  simulation_size = 1000,
   batch_size = 48,
   n_cores = 24)
 
