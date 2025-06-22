07fbeb6a32326aac5f7995b571a572be9bbfafa8
Lasse Meixner <lassemeixner@gmail.com>
2025-06-22 11:02:42 +0200

    set simulation for alpha = 0.5

--- simulation/main_simulation.R
+++ simulation/main_simulation.R
@@ -10,14 +10,29 @@ datetime_tag <- format(Sys.time(), "%Y%m%d-%H%M")
 
 results <- run_simulation_parallel(
   # available list: "BDML-LKJ","BDML-LKJ-HP","BDML-IW","BDML-IW-HP","BDML-IW-JS-MAT","BDML-IW-JS-I","BLRs-baseline","BLRs-FDML","BLRs-OLS-oracle"
-  model_type = c("BDML-IW-JS-MAT", "BDML-IW-JS-I"),
+  model_type = c(
+    "BDML-IW", 
+    "BDML-IW-HP", 
+    "BDML-LKJ", 
+    "BDML-LKJ-HP", 
+    "BDML-IW-JS-I", 
+    "BDML-IW-JS-MAT", 
+    "Naive", 
+    "HCPH", 
+    "Linero", 
+    "FDML-Full", 
+    "FDML-Split", 
+    "FDML-XFit", 
+    "FDML-Alt", 
+    "OLS",
+    "Oracle"),
   n = 200,
   p = 100,
   R_Y2 = c(0, 0.4, 0.8),
   R_D2 = c(0, 0.4, 0.8),
   rho  = c(-0.5, 0, 0.5),
-  alpha= c(0.25, 1, 4),
-  simulation_size = 3000,
+  alpha = 0.5,
+  simulation_size = 500,
   batch_size = 48,
   n_cores = 24,
   datetime_tag = datetime_tag)
@@ -30,5 +45,5 @@ xtable::xtable(results_table)
 # 1. all
 generate_stacked_plots(results, datetime_tag)
 # 2. zoomed in
-#generate_stacked_plots(results, datetime_tag, zoom_in = c("BDML-IW-JS-MAT", "BDML-IW-JS-I", "BDML-IW", "Linero"))
+generate_stacked_plots(results, datetime_tag, zoom_in = c("BDML-IW-HP", "BDML-LKJ-HP", "BDML-IW-JS-MAT", "BDML-IW-JS-I", "Linero", "Oracle"))
 
